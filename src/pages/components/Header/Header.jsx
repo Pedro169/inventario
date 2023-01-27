a434677f2f6094108e84d8cad30630743f64ad78
USUARIO <pedro.tapiacast@gmail.com>
2023-01-27 18:26:45 -0400

    Primer Commit

---
+++ src/pages/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Wrapper } from "./Header.styled"
+
+
+
+export const Header = () => (
+
+    
+    <Wrapper>
+        <h1>TALLER TIC CURICO</h1>
+    </Wrapper>
+
+
+)
---
+++ src/pages/components/Header/Header.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+    display: flex;
+    flex-wrap: wrap;
+    background-color: green;
+    color: white;
+    justify-content: center;
+    padding: 40px;
+
+`;
